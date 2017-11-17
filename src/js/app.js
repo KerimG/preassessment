c4de0ba0014fdcda3543862eb7324a8ec6d25854
Kerim Güney <kerim@gueney.org>
2017-11-17 15:01:50 +0100

    initial go at the tasks

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,34 @@
-console.log('Hello World');
+new Vue({
+    el: '#app',
+
+    data: {
+        pressReleases: []
+    },
+    
+    filters: {
+        date: function (date) {
+            return moment.unix(date).format("DD.MM.YYYY HH:mm");;
+        }
+    },
+
+    mounted() {
+        axios.get('http://localhost:3000/GetLocalPressReleases')
+            .then(response => {
+                response.data.forEach(element => {
+                    element.source = "local";
+                    this.pressReleases.push(element);
+                });
+                return axios.get('http://localhost:3000/GetGlobalPressReleases')
+            })
+            .then(response => {
+                response.data.forEach(element => {
+                    element.source = "global";
+                    this.pressReleases.push(element);
+                });
+
+                this.pressReleases.sort((x, y) => {
+                    return y.timestamp - x.timestamp;
+                });
+            });
+    }
+});
